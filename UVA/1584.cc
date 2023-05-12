13388fc14e7ec4b6c297fb6485efec5f19030357
Jeff <github@jeffteng.com>
2023-05-12 09:13:27 +0800

    add UVA 1584.

---
+++ UVA/1584.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    while (n--) {
+        string s;
+
+        // 字符串数组
+        vector<string> str_arr;
+
+        cin >> s;
+
+        // 遍历环状所有字符串
+        for (int i = 0; i < s.size(); ++i) {
+            string empty_s = "";
+
+            for (int j = 0; j < s.size(); ++j) {
+                empty_s.push_back(s[(i + j) % s.size()]);
+            }
+            str_arr.push_back(empty_s);
+        }
+
+        // 排序输出
+        sort(str_arr.begin(), str_arr.end());
+        cout << str_arr[0] << endl;
+    }
+
+    return 0;
+}
