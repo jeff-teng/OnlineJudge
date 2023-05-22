e8664e8f0e65b52cb0e34facc13dac3f30067c94
Jeff <github@jeffteng.com>
2023-05-22 17:41:40 +0800

    add UVA 1368.cc

---
+++ UVA/1368.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int T;
+
+    cin >> T;
+
+    while (T--) {
+        int n, m;
+        char s[50][1001] = {0};
+
+        scanf("%d%d", &n, &m);
+
+        for (int i = 0; i < n; ++i) {
+            scanf("%s", s[i]);
+        }
+
+        int diff = 0;
+        string result = "";
+
+        // 纵向统计
+        for (int i = 0; i < m; ++i) {
+            map<char, int> char_count;
+
+            char max_char = 'A';
+            int max_count = 0;
+            string DNA = "ACGT";
+
+            // 统计字符出现个数
+            for (int j = 0; j < n; ++j) {
+                if (char_count.count(s[j][i]) == 0) {
+                    char_count[s[j][i]] = 0;
+                }
+                ++char_count[s[j][i]];
+            }
+
+            // 统计出现最多的字符
+            for (auto c: DNA) {
+                if (char_count[c] > max_count) {
+                    max_char = c;
+                    max_count = char_count[c];
+                }
+            }
+
+            diff += n - max_count;
+            result.push_back(max_char);
+        }
+
+        printf("%s\n%d\n", result.c_str(), diff);
+    }
+
+    return 0;
+}
