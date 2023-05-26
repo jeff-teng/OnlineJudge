85824e376eae34ca66a9876ee32e59202107c406
Jeff <github@jeffteng.com>
2023-05-26 20:41:19 +0800

    add UVA 10340.

---
+++ UVA/10340.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    string s1, s2;
+
+    while (cin >> s1 >> s2) {
+        int i = 0, j = 0;
+
+        while (true) {
+            // 扫描完
+            if (i == s1.size() || j == s2.size()) {
+                break;
+            }
+
+            // 扫描 s1，s2
+            if (s1[i] == s2[j++]) {
+                ++i;
+            }
+        }
+
+        // s1 扫描完
+        if (i == s1.size()) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
+    }
+
+    return 0;
+}
